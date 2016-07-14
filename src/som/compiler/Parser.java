da1f5b33d33b46cc1c34e0591bb88c275e43dd81
Dardo Marasca <gefarion@gmail.com>
2016-07-14 09:36:26 -0300

    Cascade method logic refactoring

--- src/som/compiler/Parser.java
+++ src/som/compiler/Parser.java
@@ -545,7 +545,30 @@ public class Parser {
   private String makeTempVariableName(ExpressionNode receiver) {
     SourceSection sourceSection = receiver.getSourceSection();
 
-    return "methodCascade@" + sourceSection.getSource().getShortName() + ":" + sourceSection.getStartLine() + ":" + sourceSection.getStartColumn();
+    return "methodCascade@" + sourceSection.getSource().getShortName() + ":" + sourceSection.getStartLine()
+      + ":" + sourceSection.getStartColumn();
+  }
+
+  private ExpressionNode cascadeMessages(
+    final MethodGenerationContext mgenc, ExpressionNode firstMessage, ExpressionNode receiver,
+    SourceCoordinate coord, SourceSection section) throws ParseError {
+
+    String varname = makeTempVariableName(receiver);
+    mgenc.addLocalIfAbsent(varname);
+
+    firstMessage.adoptChildren();
+    receiver.replace(variableRead(mgenc, varname, section));
+
+    List<ExpressionNode> expressions = new ArrayList<ExpressionNode>();
+
+    expressions.add(variableWrite(mgenc, varname, receiver, section));
+    expressions.add(firstMessage);
+
+    while (accept(SemiColon)) {
+      expressions.add(messages(mgenc, variableRead(mgenc, varname, section)));
+    }
+
+    return createSequenceNode(coord, expressions);
   }
 
   private ExpressionNode evaluation(final MethodGenerationContext mgenc) throws ParseError {
@@ -560,24 +583,7 @@ public class Parser {
       exp = messages(mgenc, receiver);
 
       if (SemiColon == sym) {
-        // Method Cascade
-
-        String varname = makeTempVariableName(receiver);
-        mgenc.addLocalIfAbsent(varname);
-
-        exp.adoptChildren();
-        receiver.replace(variableRead(mgenc, varname, section));
-
-        List<ExpressionNode> expressions = new ArrayList<ExpressionNode>();
-
-        expressions.add(variableWrite(mgenc, varname, receiver, section));
-        expressions.add(exp);
-
-        while (accept(SemiColon)) {
-          expressions.add(messages(mgenc, variableRead(mgenc, varname, section)));
-        }
-
-        return createSequenceNode(coord, expressions);
+        return cascadeMessages(mgenc, exp, receiver, coord, section);
       }
 
     }
