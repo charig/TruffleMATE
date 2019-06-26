40aed88f2035e643ff0ea4a0b3279f1f2811d4fb
Guido Chari <charig@gmail.com>
2019-06-26 15:28:47 +0200

    Add missing primitive

---
+++ src/som/primitives/arrays/AsStringPrim.java
@@ -0,0 +1,54 @@
+package som.primitives.arrays;
+
+import com.oracle.truffle.api.dsl.GenerateNodeFactory;
+import com.oracle.truffle.api.dsl.ImportStatic;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.instrumentation.Tag;
+import com.oracle.truffle.api.profiles.ValueProfile;
+
+import bd.primitives.Primitive;
+import som.interpreter.nodes.nary.UnaryExpressionNode;
+import som.vmobjects.SArray;
+import som.vmobjects.SArray.ArrayType;
+import tools.dym.Tags.ArrayRead;
+import tools.dym.Tags.BasicPrimitiveOperation;
+
+@GenerateNodeFactory
+@Primitive(className = "Array", primitive = "asString", selector = "asString", receiverType = SArray.class)
+@ImportStatic(ArrayType.class)
+public abstract class AsStringPrim extends UnaryExpressionNode {
+  private final ValueProfile storageType = ValueProfile.createClassProfile();
+
+  @Specialization(guards = "isEmptyType(receiver)")
+  public final String doEmptySArray(final SArray receiver) {
+    return "";
+  }
+
+  @Specialization(guards = "isCharType(receiver)")
+  public final String doCharSArray(final SArray receiver) {
+    return String.valueOf(receiver.getCharStorage(storageType));
+  }
+
+  @Specialization
+  public final String doObject(final SArray receiver) {
+    String str = "";
+
+    for (Object elem : receiver.getObjectStorage(storageType)) {
+      str += elem.toString();
+    }
+    return  str;
+  }
+
+
+
+  @Override
+  protected boolean hasTagIgnoringEagerness(final Class<? extends Tag> tag) {
+    if (tag == BasicPrimitiveOperation.class) {
+      return true;
+    } else if (tag == ArrayRead.class) {
+      return true;
+    } else {
+      return super.hasTagIgnoringEagerness(tag);
+    }
+  }
+}
